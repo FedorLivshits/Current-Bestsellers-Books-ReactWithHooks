9478e33def0753855e57c4f49ada5adb44349520
FedorLivshits <fediliv@gmail.com>
2021-04-16 20:25:26 +0300

    added full css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import './App.css';
+import './reset.css'
 import BooksApp from "./BooksApp";
 
 function App() {
--- src/BooksApp.jsx
+++ src/BooksApp.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react"
 import axios from "axios";
 
-const REACT_BOOKS_APP_API_KEY= `Fmu44FKGd9BpwsmFi4JAd5qwxyzsQjyl`
+const REACT_BOOKS_APP_API_KEY = `Fmu44FKGd9BpwsmFi4JAd5qwxyzsQjyl`
 
 function BooksApp() {
     const [books, setBooks] = useState([])
@@ -17,25 +17,38 @@ function BooksApp() {
     }, [])
     return (
         <section className="current__books-app">
+
             <div className="container">
                 <section className="app__inner">
                     <h1 className="title">
                         Actual books bestsellers
                     </h1>
+                    <div className="current-date">
+                        {new Date().toLocaleString('en', {weekday: 'long'})}
+                        {" "}
+                        {new Date().getDate()}
+                        {" "}
+                        {new Date().toLocaleString('en', {month: 'long'})}
+                        {" "}
+                    </div>
                     <div className="content">
                         {books.map(book => {
-                            const {rank, description, title, author, book_image, buy_links, publisher} = book
-                            return (
-                                <article key={rank}>
-                                        <img src={book_image} alt={title}/>
-                                    <div className="book-descr">
-                                        <h2 className="book-title">
-                                            <span>{rank}</span>. {title}
-                                        </h2>
-                                        <p>{description}</p>
-                                    </div>
-                                </article>
-                            )
+                                const {rank, description, title, author, book_image, buy_links, publisher} = book
+                                return (
+                                    <article className="book-card" key={rank}>
+                                        <div className="book-img">
+                                            <img src={book_image} alt={title}/>
+                                        </div>
+                                        <div className="book-descr">
+                                            <h2 className="book-title">
+                                                <span>{rank}</span>. {title}
+                                            </h2>
+                                            <p>By: {author}</p>
+                                            <p>{description}</p>
+
+                                        </div>
+                                    </article>
+                                )
                             }
                         )}
                     </div>
