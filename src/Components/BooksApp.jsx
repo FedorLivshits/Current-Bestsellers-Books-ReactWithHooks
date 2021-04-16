076c05ec64bea6d0a1e21e0ce992bb88f115fa3d
FedorLivshits <fediliv@gmail.com>
2021-04-16 23:15:03 +0300

    refactor css

--- src/Components/BooksApp.jsx
+++ src/Components/BooksApp.jsx
@@ -23,19 +23,19 @@ function BooksApp() {
     return (
         <section className="current__books-app">
             <header className="header">
-                <h1 className="title">
-                    Actual <br/>
-                    books <br/>
-                    bestsellers<br/>
-                    <div className="current-date">
-                        on {new Date().toLocaleString('en', {weekday: 'long'})}
-                        {" "}
-                        {new Date().getDate()}
-                        {" "}
-                        {new Date().toLocaleString('en', {month: 'long'})}
-                        {" "}
-                    </div>
-                </h1>
+                    <h1 className="title">
+                        Actual <br/>
+                        books <br/>
+                        bestsellers<br/>
+                        <div className="current-date">
+                            on {new Date().toLocaleString('en', {weekday: 'long'})}
+                            {" "}
+                            {new Date().getDate()}
+                            {" "}
+                            {new Date().toLocaleString('en', {month: 'long'})}
+                            {" "}
+                        </div>
+                    </h1>
             </header>
             <div className="container">
                 {isLoading
